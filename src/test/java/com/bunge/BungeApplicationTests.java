38f2bcc9651b4423b0fa5a98fb402246d9dffbe7
kjh <kjh4940@naver.com>
2024-05-16 15:20:56 +0900

    Create project

---
+++ src/test/java/com/bunge/BungeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BungeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
