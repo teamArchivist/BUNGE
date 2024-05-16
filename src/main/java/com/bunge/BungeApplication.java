48479dcf9177b372b3fcf0d7f755371af2fde764
kjh <kjh4940@naver.com>
2024-05-16 15:20:56 +0900

    Create project

---
+++ src/main/java/com/bunge/BungeApplication.java
@@ -0,0 +1,13 @@
+package com.bunge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BungeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BungeApplication.class, args);
+    }
+
+}
