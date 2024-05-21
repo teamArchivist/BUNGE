710052cf8320219a09d8c82c7bc81c40bdd860e9
gamjoo <yyy970309@gmail.com>
2024-05-21 17:36:13 +0900

    [feat] "header 작성"
    header_sidenav.html 작성. Thymeleaf 태그로 변경 후 테스트
    
    Ref : #22
    Related to : #22

--- src/main/java/com/bunge/review/controller/ReviewController.java
+++ src/main/java/com/bunge/review/controller/ReviewController.java
@@ -1,15 +1,42 @@
 package com.bunge.review.controller;
 
+import com.bunge.review.service.ReviewService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value="/review")
 public class ReviewController {
 
     private static final Logger logger = LoggerFactory.getLogger(ReviewController.class);
+
+    private ReviewService reviewservice;
+
+    @Autowired
+    public ReviewController(ReviewService reviewservice) {
+        this.reviewservice = reviewservice;
+    }
+
+    @RequestMapping(value="/main", method= RequestMethod.GET)
+    public ModelAndView reviewList(@RequestParam(value="page", defaultValue="1") int page,
+                                   ModelAndView mv) {
+
+        mv.setViewName("review/header_sidenav");
+
+        return mv;
+
+    }
+
+
+
+
+
 
 
 }
