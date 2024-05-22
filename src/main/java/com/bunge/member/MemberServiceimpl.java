d25708a5974f1413aa4a5a8d045306ea9e67c937
kjh <kjh4940@naver.com>
2024-05-22 10:50:34 +0900

    member 패키지 에러 주석처리

--- src/main/java/com/bunge/member/MemberServiceimpl.java
+++ src/main/java/com/bunge/member/MemberServiceimpl.java
@@ -4,16 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Service
-public class MemberServiceimpl implements MemberService{
-
-    private MemberMapper dao;
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    public MemberServiceimpl(MemberMapper dao, PasswordEncoder passwordEncoder) {
-        this.dao = dao;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-}
+//@Service
+//public class MemberServiceimpl implements MemberService{
+//
+//    private MemberMapper dao;
+//    private PasswordEncoder passwordEncoder;
+//
+//    @Autowired
+//    public MemberServiceimpl(MemberMapper dao, PasswordEncoder passwordEncoder) {
+//        this.dao = dao;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+//
+//}
