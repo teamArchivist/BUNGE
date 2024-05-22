f3e74de2f7b2140ff26484c4710cfe84c23352eb
kjh <kjh4940@naver.com>
2024-05-22 10:59:35 +0900

    resolve merge conflict

--- src/main/java/com/bunge/review/domain/Review.java
+++ src/main/java/com/bunge/review/domain/Review.java
@@ -1,7 +1,85 @@
 package com.bunge.review.domain;
 
 public class Review {
-    private int no;
+    private int reviewno;
+    private String userid;
+    private int bookno;
+    private String linetitle;
+    private String title;
+    private String content;
+    private int rating;
+    private String createdat;
+    private int challenge;
 
+    public int getReviewno() {
+        return reviewno;
+    }
 
+    public void setReviewno(int reviewno) {
+        this.reviewno = reviewno;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public int getBookno() {
+        return bookno;
+    }
+
+    public void setBookno(int bookno) {
+        this.bookno = bookno;
+    }
+
+    public String getLinetitle() {
+        return linetitle;
+    }
+
+    public void setLinetitle(String linetitle) {
+        this.linetitle = linetitle;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getCreatedat() {
+        return createdat;
+    }
+
+    public void setCreatedat(String createdat) {
+        this.createdat = createdat;
+    }
+
+    public int getChallenge() {
+        return challenge;
+    }
+
+    public void setChallenge(int challenge) {
+        this.challenge = challenge;
+    }
 }
