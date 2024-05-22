3c71421ba95e2dc7f920d2fba54657afe6e7d630
gamjoo <yyy970309@gmail.com>
2024-05-23 08:21:35 +0900

    [feat] "review 메인페이지 제작중"
    review_main.html 뷰페이지, 북커버 이미지 추가, 컨트롤러 매핑, Mapper/Service 구조 (progress)
    
    Ref : #34
    Related to : #34

--- src/main/java/com/bunge/memo/controller/MemoController.java
+++ src/main/java/com/bunge/memo/controller/MemoController.java
@@ -1,4 +1,33 @@
 package com.bunge.memo.controller;
 
+import com.bunge.memo.service.MemoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="/memo")
 public class MemoController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MemoController.class);
+
+    private MemoService memoservice;
+
+    @Autowired
+    public MemoController(MemoService memoservice) { this.memoservice = memoservice; }
+
+    @RequestMapping(value="/main")
+    public ModelAndView memoList(@RequestParam(value="page", defaultValue="1") int page,
+                                 ModelAndView mv) {
+
+        mv.setViewName("memo/memo_mine");
+
+        return mv;
+
+    }
+
 }
