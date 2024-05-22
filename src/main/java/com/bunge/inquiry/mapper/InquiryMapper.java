be0593375d669b93a6e4e87b00314ebc114caba9
Hwon00 <nak173@naver.com>
2024-05-22 17:43:03 +0900

    [feat] : mapper.xml,service 파일 추가2(오후 작업)
    파일추가

--- src/main/java/com/bunge/inquiry/mapper/InquiryMapper.java
+++ src/main/java/com/bunge/inquiry/mapper/InquiryMapper.java
@@ -2,18 +2,16 @@ package com.bunge.inquiry.mapper;
 
 import com.bunge.inquiry.domain.Inquiry;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface InquiryMapper {
     void insertInquiry(Inquiry inquiry);
-
-    void updateInquiry(Inquiry inquiry);
-
-    void deleteInquiry(int id);
-
-    Inquiry selectInquiryById(int id);
-
-    List<Inquiry> selectInquiries(Integer typeId);
+    void updateInquiry(@Param("inquiry") Inquiry inquiry);
+    void deleteInquiry(@Param("inquiryId") int inquiryId, @Param("memberId") String memberId);
+    Inquiry selectInquiry(int inquiryId);
+    List<Inquiry> selectInquiriesByType(int typeId);
 }
+
--- src/main/java/com/bunge/inquiry/service/InquiryService.java
+++ src/main/java/com/bunge/inquiry/service/InquiryService.java
@@ -5,6 +5,9 @@ import com.bunge.inquiry.domain.Inquiry;
 import java.util.List;
 
 public interface InquiryService {
-
+    void createInquiry(Inquiry inquiry);
+    void updateInquiry(Inquiry inquiry);
+    void deleteInquiry(int inquiryId, String memberId);
+    Inquiry getInquiryById(int inquiryId);
+    List<Inquiry> getInquiriesByType(int typeId);
 }
-
