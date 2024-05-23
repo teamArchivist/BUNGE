c8bb8e81cfe520baaafc1cfc65c96a1a3ca10b10
gamjoo <yyy970309@gmail.com>
2024-05-23 17:11:10 +0900

    [feat] "memo 메인페이지 제작중"
    memo_mine.html 뷰페이지, chart.js 추가,  memo 관련 컨트롤러 매핑, Mapper/Service 구조 (progress)
    
    Ref : #34
    Related to : #34

--- src/main/java/com/bunge/memo/controller/MemoController.java
+++ src/main/java/com/bunge/memo/controller/MemoController.java
@@ -1,10 +1,15 @@
 package com.bunge.memo.controller;
 
+import com.bunge.memo.domain.Memo;
 import com.bunge.memo.service.MemoService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,14 +25,17 @@ public class MemoController {
     @Autowired
     public MemoController(MemoService memoservice) { this.memoservice = memoservice; }
 
-    @RequestMapping(value="/main")
+    //기록·리뷰 -> 나의 기록 눌렀을 때 처음 페이지
+    @GetMapping(value="/mine")
     public ModelAndView memoList(@RequestParam(value="page", defaultValue="1") int page,
                                  ModelAndView mv) {
 
         mv.setViewName("memo/memo_mine");
 
         return mv;
-
     }
+
+
+
 
 }
