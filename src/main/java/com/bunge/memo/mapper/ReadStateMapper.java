e0fc0090605dc98c614f535ab15b9d5acdc2e245
gamjoo <yyy970309@gmail.com>
2024-06-05 00:15:45 +0900

    [fix] 기록 삭제했을 때 완독 상태가 유지되는 에러 수정
    작성한 기록을 삭제했을 때, 총 페이지보다 남아 있는 페이지가 작다면 독서상태가 완독에서 도전으로 변경되도록 에러 수정

--- src/main/java/com/bunge/memo/mapper/ReadStateMapper.java
+++ src/main/java/com/bunge/memo/mapper/ReadStateMapper.java
@@ -1,5 +1,6 @@
 package com.bunge.memo.mapper;
 
+import com.bunge.memo.domain.Memo;
 import com.bunge.memo.domain.ReadState;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -17,6 +18,9 @@ public interface ReadStateMapper {
     //로그인한 사용자의 모든 독서상태
     public List<ReadState> getAllReadState(String loginId);
 
+    //기록에 대응되는 독서상태 조회
+    public ReadState getReadState(Memo memo);
+
     public int countTotalPage(ReadState readState);
 
     public int countReadPage(ReadState readState);
@@ -24,7 +28,7 @@ public interface ReadStateMapper {
     //독서상태 업데이트
     public void updateReadState(ReadState readState);
 
-    //목표도서 도전도서로 변경
+    //독서상태 변경
     public int changeReadState(ReadState readState);
 
 }
