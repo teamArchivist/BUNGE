b4188b167ddeefb496681019ff1af0cdb6049519
kjh <kjh4940@naver.com>
2024-06-10 17:15:16 +0900

    [feat] Stomp-Kafka 연동 #139 - Kafka에 Produce, Consume 처리하도록 MessageController 변경 - Stomp와 Kafka 발행/구독 경로 맵핑

--- src/main/java/com/bunge/chat/config/WebSocketConfig.java
+++ src/main/java/com/bunge/chat/config/WebSocketConfig.java
@@ -18,7 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
         registry.setApplicationDestinationPrefixes("/pub");
     }
 }
--- src/main/java/com/bunge/chat/service/KafkaConsumerService.java
+++ src/main/java/com/bunge/chat/service/KafkaConsumerService.java
@@ -1,18 +1,23 @@
 package com.bunge.chat.service;
 
 import com.bunge.chat.KafkaTopic;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class KafkaConsumerService {
 
-    @KafkaListener(topics = KafkaTopic.TEMP, groupId = "kafka-consumer-group")
-    public void handleMessage(String message, Acknowledgment ack) {
+    private final SimpMessagingTemplate template;
+
+    public void handleMessage(String topic, String message, Acknowledgment ack) {
         log.info("consumer message={}", message);
+        template.convertAndSend("/" + topic, message);
         ack.acknowledge();
     }
 }
