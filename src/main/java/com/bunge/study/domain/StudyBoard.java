8ae5e86f3877f701a996f844ab39c04d1f4a7f65
gamjoo <yyy970309@gmail.com>
2024-06-11 15:13:26 +0900

    [feat] 스터디 기록 리스트 (read)
    스터디 기록 생성 후 글 목록을 보여주는 view 페이지 관련 작업
    
    Resolves : #194

---
+++ src/main/java/com/bunge/study/domain/StudyBoard.java
@@ -0,0 +1,24 @@
+package com.bunge.study.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+public class StudyBoard {
+    private int no;
+    private String id;
+    private String booktitle;
+    private String bookcover;
+    private String title;
+    private String content;
+    private String startdate;
+    private String enddate;
+    private String challengestart;
+    private String challengeend;
+    private int quota;
+    private int state;
+    private int readcount;
+}
