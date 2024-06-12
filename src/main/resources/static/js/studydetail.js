dff7c24866b34fe868a66c68f9ae61d1ec5ec041
gamjoo <yyy970309@gmail.com>
2024-06-12 23:40:56 +0900

    [refactor] 스터디 모집글 댓글 보기 리팩토링
    댓글 추가하면 바로 해당 리스트가 보이면서 총 댓글 수가 동적으로 변하도록 개선함

--- src/main/resources/static/js/studydetail.js
+++ src/main/resources/static/js/studydetail.js
@@ -1,4 +1,7 @@
 $(function () {
+    let studyboardno = $("#commentAddBtn").data("studyboardno")
+
+    getStudyCommentList(studyboardno)
 
     let countStudyComm = $("#countStudyComm").val();
     console.log(countStudyComm)
@@ -7,10 +10,9 @@ $(function () {
         let commentsSection = $("#commentsSection");
         if (commentsSection.is(":visible")) {
             commentsSection.hide();
-            $(this).text("댓글 보기 (" + countStudyComm + ")");
+            getStudyCommentList()
         } else {
             commentsSection.show();
-            $(this).text("댓글 가리기 (" + countStudyComm + ")");
         }
     });
 
@@ -39,8 +41,8 @@ $(function () {
                 success: function(response) {
                     //console.log(response)
                     if (response.status === "success") {
-                        alert(response.message)
                         getStudyCommentList(studyboardno);
+                        alert(response.message)
                         contentselector.val('')
                     } else {
                         alert(response.message)
@@ -61,8 +63,16 @@ $(function () {
             url: "/study/get-board-comment",
             method: "post",
             data: { no : studyboardno },
+            beforeSend : function (xhr) {
+                if (header && token) {
+                    xhr.setRequestHeader(header, token);
+                }
+            },
             success: function (response) {
-                console.log(response)
+                if (response.studyCommList) {
+                    renderComments(response.studyCommList)
+                    $("#toggleComments").text("댓글 보기 (" + response.studyCommListCount + ")");
+                }
             },
             error: function (status, error) {
                 console.log("ajax 요청 실패")
@@ -71,5 +81,37 @@ $(function () {
             }
         })
     }
-})
+
+    function renderComments(comments) {
+        let commentList = $('#commentList');
+        commentList.empty(); // 기존 댓글 삭제
+
+        comments.forEach(comment => {
+            let commentHtml = `
+                <div class="d-flex mb-2">
+                    <div class="flex-shrink-0">
+                        <img class="img-sm rounded-circle" src="/img/profile-photos/10.png" alt="Profile" loading="lazy">
+                    </div>
+                    <div class="flex-grow-1 ms-3">
+                        <div class="mb-1">
+                            <a class="h6 btn-link">${comment.id}</a>
+                            <small class="ms-2 text-body-secondary mb-0">${comment.created}</small>
+                            &nbsp;
+                            <button class="btn btn-icon btn-outline-primary btn-xs rounded-circle">
+                                <ion-icon name="settings-outline"></ion-icon>
+                            </button>
+                            <button class="btn btn-icon btn-outline-warning btn-xs rounded-circle">
+                                <ion-icon name="trash-outline"></ion-icon>
+                            </button>
+                        </div>
+                        <span>${comment.content}</span>
+                        <a class="btn btn-xs btn-outline-light">Reply</a>
+                    </div>
+                </div>`;
+
+            commentList.append(commentHtml);
+        });
+    }
+
+}) //ready end
 
