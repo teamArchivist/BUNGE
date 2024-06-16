40ccf809cc051d9ada5fa5716033ed3248e242d0
Jeonghyo Kim <kjh4940@naver.com>
2024-06-16 15:41:58 +0900

    [feat] 채팅 임시 생성 페이지 구현

--- src/main/java/com/bunge/chat/controller/ChatController.java
+++ src/main/java/com/bunge/chat/controller/ChatController.java
@@ -1,17 +1,30 @@
 package com.bunge.chat.controller;
 
+import com.bunge.member.domain.Member;
+import com.bunge.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/chat")
 public class ChatController {
+
+    private final MemberService memberService;
 
     @GetMapping
     public String chatPage() {
         return "chat/chat";
+    }
+
+    @GetMapping("/rooms")
+    public String showCreatePage(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "chat/chat-enter";
     }
 }
