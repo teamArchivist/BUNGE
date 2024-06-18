aee2f4748edab85c49ce2c488d64170523c36fc3
kjh <kjh4940@naver.com>
2024-06-18 09:18:20 +0900

    [feat] 컨트롤러 채팅 생성 호출 기능 추가 #188

--- src/main/java/com/bunge/chat/controller/ChatController.java
+++ src/main/java/com/bunge/chat/controller/ChatController.java
@@ -1,10 +1,11 @@
 package com.bunge.chat.controller;
 
-import com.bunge.chat.domain.Chatroom;
+import com.bunge.chat.domain.ChatRequestDto;
 import com.bunge.chat.service.ChatService;
 import com.bunge.member.domain.Member;
 import com.bunge.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +17,25 @@ import java.util.List;
 @RequestMapping("/chat")
 public class ChatController {
 
+    private final ChatService service;
     private final MemberService memberService;
 
     @GetMapping
     public String showChatMainPage() {
         return "chat/chat";
+    }
+
+    @PostMapping("/rooms")
+    public String createChat(@AuthenticationPrincipal Member loginMember,
+                             @ModelAttribute Member member) {
+        // TODO: 사용자 검증 로직 구현
+        ChatRequestDto requestDto = ChatRequestDto.builder()
+                .loginId(loginMember.getId())
+                .memberId(member.getId())
+                .state(1)
+                .build();
+        ChatRequestDto chatroom = service.createChatroom(requestDto);
+        return "redirect:/chat/rooms/" + chatroom.getChatroomId();
     }
 
     @GetMapping("/rooms")
