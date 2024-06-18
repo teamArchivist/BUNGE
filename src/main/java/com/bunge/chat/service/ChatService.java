c15bfaab2ebb2eb5dbeef4017612818743128497
kjh <kjh4940@naver.com>
2024-06-18 09:15:16 +0900

    [feat] 채팅 Service 생성 #188 - 채팅방 생성 로직 추가 (채팅방 + 관계)

---
+++ src/main/java/com/bunge/chat/service/ChatService.java
@@ -0,0 +1,24 @@
+package com.bunge.chat.service;
+
+import com.bunge.chat.domain.ChatListDto;
+import com.bunge.chat.domain.ChatRequestDto;
+import com.bunge.chat.repository.ChatRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+
+    private final ChatRepository repository;
+
+    @Transactional
+    public ChatRequestDto createChatroom(ChatRequestDto requestDto) {
+        ChatRequestDto save = repository.save(requestDto);
+        repository.saveRelation(requestDto);
+        return save;
+    }
+}
