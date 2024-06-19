82c373d7d71a23da4c767f66c7c4a125715e0754
kjh <kjh4940@naver.com>
2024-06-19 15:42:22 +0900

    [feat] Json Mapper 클래스 LocalDateTime 처리 가능하게 변경

--- src/main/java/com/bunge/chat/util/JsonUtils.java
+++ src/main/java/com/bunge/chat/util/JsonUtils.java
@@ -3,14 +3,21 @@ package com.bunge.chat.util;
 import com.bunge.chat.domain.Message;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JsonUtils {
 
     public static String getString(Object object) throws JsonProcessingException {
-        return new ObjectMapper().writeValueAsString(object);
+        return objectMapper().writeValueAsString(object);
     }
 
     public static Message getObject(String payload) throws JsonProcessingException {
-        return new ObjectMapper().readValue(payload, Message.class);
+        return objectMapper().readValue(payload, Message.class);
+    }
+
+    static ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 }
