e19e8640fc8bc168271540a65619b8ee613b0aab
Hwon00 <nak173@naver.com>
2024-06-28 11:17:52 +0900

    [fix] 문의글 댓글 프로필 경로 수정
    문의글 댓글 프로필 경로 수정
    
    Resolves: #109

--- src/main/java/com/bunge/inquiry/controller/InquiryCommentController.java
+++ src/main/java/com/bunge/inquiry/controller/InquiryCommentController.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
